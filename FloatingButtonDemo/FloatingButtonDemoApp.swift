c05b85560680e7e4349676dc05aa40803af60754
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2024-12-03 17:17:39 +0530

    Initial Commit

---
+++ FloatingButtonDemo/FloatingButtonDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingButtonDemoApp.swift
+//  FloatingButtonDemo
+//
+//  Created by Bhumika Patel on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FloatingButtonDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FloatingButtonDemoTests/FloatingButtonDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingButtonDemoTests.swift
+//  FloatingButtonDemoTests
+//
+//  Created by Bhumika Patel on 03/12/24.
+//
+
+import Testing
+@testable import FloatingButtonDemo
+
+struct FloatingButtonDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
