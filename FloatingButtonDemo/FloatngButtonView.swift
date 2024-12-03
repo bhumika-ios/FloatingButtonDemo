df8d612c2fe534424ac8d2fcd07894d8dd36b6fb
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2024-12-03 18:00:49 +0530

    create for floating button file

---
+++ FloatingButtonDemo/FloatngButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  FloatngButtonView.swift
+//  FloatingButtonDemo
+//
+//  Created by Bhumika Patel on 03/12/24.
+//
+
+import SwiftUI
+
+struct FloatngButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FloatngButtonView()
+}
